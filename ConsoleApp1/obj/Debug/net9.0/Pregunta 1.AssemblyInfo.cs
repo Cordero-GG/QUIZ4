f0c9dfec0b6f31b6004fd0631c61fa48a817beee
Cordero-GG <d.cordero.1@estudiantec.cr>
2025-05-29 22:46:03 -0600

    códigos

--- ConsoleApp1/obj/Debug/net9.0/Pregunta 1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/Pregunta 1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Pregunta 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+008771255070ecbcc7979d7d20d4681dc08c08bc")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Pregunta 1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Pregunta 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
---
+++ Pregunta 2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+class DiccionarioSimple
+{
+    private List<Tuple<char, int>> conteoCaracteres;
+
+    public DiccionarioSimple()
+    {
+        conteoCaracteres = new List<Tuple<char, int>>();
+    }
+
+    // Agrega una clave o incrementa su conteo si ya existe
+    public void Agregar(char clave)
+    {
+        for (int i = 0; i < conteoCaracteres.Count; i++)
+        {
+            if (conteoCaracteres[i].Item1 == clave)
+            {
+                conteoCaracteres[i] = Tuple.Create(clave, conteoCaracteres[i].Item2 + 1);
+                return;
+            }
+        }
+        // Si no existe, la agregamos con conteo = 1
+        conteoCaracteres.Add(Tuple.Create(clave, 1));
+    }
+
+    // Remueve una clave o decrementa su conteo si existe
+    public void Remover(char clave)
+    {
+        for (int i = 0; i < conteoCaracteres.Count; i++)
+        {
+            if (conteoCaracteres[i].Item1 == clave)
+            {
+                conteoCaracteres[i] = Tuple.Create(clave, conteoCaracteres[i].Item2 - 1);
+                return;
+            }
+        }
+        // Si no existe, la agregamos con conteo = -1
+        conteoCaracteres.Add(Tuple.Create(clave, -1));
+    }
+
+    // Verifica si todos los conteos son cero
+    public bool TodosCeros()
+    {
+        foreach (var par in conteoCaracteres)
+        {
+            if (par.Item2 != 0)
+                return false;
+        }
+        return true;
+    }
+}
+
+class Program
+{
+    static bool SonAnagramas(string s1, string s2)
+    {
+        if (s1.Length != s2.Length)
+            return false;
+
+        DiccionarioSimple diccionario = new DiccionarioSimple();
+
+        // Contamos las letras del primer string
+        foreach (char c in s1)
+        {
+            diccionario.Agregar(c);
+        }
+
+        // Restamos las letras del segundo string
+        foreach (char c in s2)
+        {
+            diccionario.Remover(c);
+        }
+
+        // Si todos los conteos son cero, son anagramas
+        return diccionario.TodosCeros();
+    }
+
+    static void Main()
+    {
+        string palabra1 = "listen";
+        string palabra2 = "silent";
+
+        if (SonAnagramas(palabra1, palabra2))
+        {
+            Console.WriteLine("¡Son anagramas!");
+        }
+        else
+        {
+            Console.WriteLine("No son anagramas.");
+        }
+    }
+}
